5fac9256e9ddc65cf8c10e932cc17576c0768d23
user2 <user2@KH_C>
2019-02-12 17:40:27 +0900

    model.vo추가

---
+++ src/model/vo/Ball.java
@@ -0,0 +1,37 @@
+package model.vo;
+
+import java.awt.Image;
+
+public class Ball extends Item {
+	
+	private int cProb;	//포켓몬 잡는 확률
+	private int cType;	//볼 종류 -> 0 : 몬스터볼 / 1 : 슈퍼볼 / 2 : 마스터볼
+	
+	
+	public Ball() {}
+	
+	public Ball(String iName, int iNo, int iPrice, int iType, Image iImg, int cProb, int cType) {
+		super(iName, iNo, iPrice, iType, iImg);
+		this.cProb = cProb;
+		this.cType = cType;
+	}
+
+	public int getcProb() {
+		return cProb;
+	}
+
+	public int getcType() {
+		return cType;
+	}
+
+	public void setcProb(int cProb) {
+		this.cProb = cProb;
+	}
+
+	public void setcType(int cType) {
+		this.cType = cType;
+	}
+	
+	
+
+}
