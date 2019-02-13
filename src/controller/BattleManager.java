f262f67b74810eb1e7751c330994a89daeba7926
rndwh <rndwh@DESKTOP-PKLTO7S>
2019-02-13 20:07:06 +0900

    머니머니해도 머지

--- src/controller/BattleManager.java
+++ src/controller/BattleManager.java
@@ -2,9 +2,12 @@ package controller;
 
 import java.util.Random;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JTextArea;
 
 import model.dao.PokemonDao;
+import model.dao.SkillDao;
 import model.dao.UserDao;
 import model.vo.Pokemon;
 import view.PInfoPage;
@@ -15,15 +18,30 @@ public class BattleManager {
 	private static UserDao ud = new UserDao();
 	private static PokemonDao pd = new PokemonDao();
 	private static PInfoPage pip;
+	private static Pokemon poke;
+	private static SkillDao sd;
 
+	public static Pokemon getPoke() {
+		return poke;
+	}
+
+	public static void setPoke(Pokemon poke) {
+		BattleManager.poke = poke;
+	}
 
 	//현재 포켓몬 보여주는 부분
 	public static void showP(PInfoPage pip) {
 		BattleManager.pip = pip;
 
 		JTextArea[] jta = new JTextArea[4];
+		JLabel[] jl = new JLabel[4];
 
 		for(int i=0; i<ud.getUp_list().size(); i++) {
+
+
+			jl[i] = new JLabel();
+			jl[i].setIcon(new ImageIcon(ud.getUp_list().get(i).getpImg()));
+
 			jta[i] = new JTextArea();
 			jta[i].setText("\t포켓몬 이름 : "+ ud.getUp_list().get(i).getpName() + "\n"
 					+"\t레    벨 : " + ud.getUp_list().get(i).getpLevel() + "\n"
@@ -31,9 +49,13 @@ public class BattleManager {
 					+"\t스 피 드 : " + ud.getUp_list().get(i).getpSpeed() + "\n"
 					+"\t체    력  : " +ud.getUp_list().get(i).getpHp() + "\n");
 			jta[i].setEditable(false);
+
+
+
 		}
 
 		pip.setpInfoText(jta);
+		pip.setpInfo(jl);
 
 
 	}
@@ -42,10 +64,12 @@ public class BattleManager {
 	public static void randomP() {
 
 
+
 		int randomIndex = new Random().nextInt(4)+1;
 		int randomLevel = new Random().nextInt(10)+1;
-		Pokemon poke = pd.getpList().get(randomIndex);
+		poke = pd.getpList().get(randomIndex);
 
+		//pd.setNum(poke.getpNo());
 
 		//포켓몬 속성 정의
 		poke.setpLevel(randomLevel);
@@ -54,15 +78,43 @@ public class BattleManager {
 		poke.setpSpeed(randomLevel);
 		poke.setExp(randomLevel);
 
-		for(int i=0; i<4; i++) {
+		
+		for(int i =0; i<4; i++) {
+			int random = new Random().nextInt(4);
+			if(poke.getpType() == 0) {
+			}
 		}
+		
+
+	}
+
+	//스킬 클릭했을 때
+	public static void selectS() {
 
 	}
 
 
+	//내 포켓몬이 공격
+	public static void atkMP(UserDao user, Pokemon poke) {
+
+	}
+
+	//상대 포켓몬이 공격
+	public static void atkEP(UserDao user, Pokemon poke) {
+
+		Pokemon myPoke= user.getUp_list().get(0);
+
+		if(poke.getpSpeed() > myPoke.getpSpeed()) {
+			int random = new Random().nextInt(4);
+			poke.getpSkill().get(random);
 
 
 
+		}
+
+
+
+	}
 
 
 	//포켓몬 캐치 기능
@@ -75,9 +127,7 @@ public class BattleManager {
 		}
 	}
 
-	public static void selectS() {
 
-	}
 
 
 
--- src/model/dao/SkillDao.java
+++ src/model/dao/SkillDao.java
@@ -7,13 +7,70 @@ import model.vo.Skill;
 
 public class SkillDao {
 
-	private List<Skill> ps_list;
+	private List<Skill> ns_list;
+	private List<Skill> fs_list;
+	private List<Skill> ws_list;
+	private List<Skill> gs_list;
 	
 	public SkillDao() {
-		ps_list.add(new Skill());
-		ps_list.add(new Skill());
-		ps_list.add(new Skill());
-		ps_list.add(new Skill());
+		
+		//³ë¸»
+		ns_list.add(new Skill("ÆİÄ¡",0,20));
+		ns_list.add(new Skill("»´¶§¸®±â",0,20));
+		ns_list.add(new Skill("ÃÊÄİ¸´¸ÔÀÌ±â",0,30));
+		//ºÒ
+		ws_list.add(new Skill("ºÒ´ë¹®ÀÚ",2,20));
+		ws_list.add(new Skill("ºÒ´ë¹®ÀÚ",2,20));
+		ws_list.add(new Skill("ºÒ´ë¹®ÀÚ",2,20));
+		ws_list.add(new Skill("ºÒ´ë¹®ÀÚ",2,20));
+		//¹°
+		fs_list.add(new Skill("ÇÏµå·ÎÆßÇÁ",1,20));
+		fs_list.add(new Skill("ÇÏµå·ÎÆßÇÁ",1,20));
+		fs_list.add(new Skill("ÇÏµå·ÎÆßÇÁ",1,20));
+		fs_list.add(new Skill("ÇÏµå·ÎÆßÇÁ",1,20));
+		
+		//Ç®
+		gs_list.add(new Skill("¼ÕÈï¹Î½¸ÆÃ",3,20));
+		gs_list.add(new Skill("¼ÕÈï¹Î½¸ÆÃ",3,20));
+		gs_list.add(new Skill("¼ÕÈï¹Î½¸ÆÃ",3,20));
+		gs_list.add(new Skill("¼ÕÈï¹Î½¸ÆÃ",3,20));
+		
 	}
+
+	public List<Skill> getFs_list() {
+		return fs_list;
+	}
+
+	public void setFs_list(List<Skill> fs_list) {
+		this.fs_list = fs_list;
+	}
+
+	public List<Skill> getWs_list() {
+		return ws_list;
+	}
+
+	public void setWs_list(List<Skill> ws_list) {
+		this.ws_list = ws_list;
+	}
+
+	public List<Skill> getGs_list() {
+		return gs_list;
+	}
+
+	public void setGs_list(List<Skill> gs_list) {
+		this.gs_list = gs_list;
+	}
+
+	public List<Skill> getNs_list() {
+		return ns_list;
+	}
+
+	public void setNs_list(List<Skill> ns_list) {
+		this.ns_list = ns_list;
+	}
+	
+	
+	
+
 	
 }
